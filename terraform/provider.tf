2cd0adcbd34396438d7c27279183f802ffa340c0
Bruno Wolff <bruno.wolff@clinicorp.com>
2023-08-30 20:03:04 -0300

    chore(main.yml): add working directory for Terraform commands to improve organization and clarity feat(provider.tf): add backend configuration for storing Terraform state in an S3 bucket in the us-east-1 region

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,12 @@ terraform {
       source = "hashicorp/aws"
     }
   }
+
+  backend "s3" {
+    bucket = "terraform-state-30082023-bruno"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
